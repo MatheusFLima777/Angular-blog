21bae14a14f7d6bdb9fa91d9b6ffe63a79af9a10
Matheus.Lima <limamatheus0010@gmail.com>
2024-02-15 23:47:33 -0300

    projeto do blog em Angular Finalizado

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,32 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description": "marvel anuncia um novo filme do homem de ferro, confira",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id":"2",
+    "title": "Nova Série anunciada no Disney +",
+    "description": "Teste ne boy",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+     "id" : "3",
+     "title": "Pantera prenha é anunciado nos cinemas",
+     "description": "Torcedores saudosistas ficam com saudades do Idolo tricolor",
+     "photoCover" : "https://img.r7.com/images/patrick-sao-paulo-sul-americana-08092022235839376?dimensions=771x420"
+  },
+    {
+      "id":"4",
+      "title":"teste",
+      "description" : "Teste",
+      "photoCover" : "https://disneyplusbrasil.com.br/wp-content/uploads/2022/10/X-Men-97-Marvel-Disney-Plus.jpg"
+    },
+    {
+      "id":"5",
+      "title":"Gabiru Mata Fabricio apos derrota do Trikas",
+      "description" : "Jovem tiltou após derrrota para o ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎ ‎‎ Fc",
+      "photoCover":"http://fferj.azurewebsites.net/admin/AzureStorage/GetImage?path=2016/4/12/cd232328-abec-4f47-9015-585e40217e36.jpg"
+    }
+   ]
